202567517a954fdf71f5cbb80baabe0e35255a04
Juan Ramos <juan-rb01@hotmail.com>
2018-12-17 15:50:07 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const router = require('./router')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+  console.log('server started...')
+
+  app.use('/', router)
+})
